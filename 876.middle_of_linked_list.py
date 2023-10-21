328abe27e3e9adb8f36b47dff95f15e84cf4f96f
GayatriDhoot20 <gdhoot202@gmail.com>
2023-10-21 17:51:52 +0530

    middle of linked list

---
+++ 876.middle_of_linked_list.py
@@ -0,0 +1,19 @@
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution(object):
+    def middleNode(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        import pdb;
+        pdb.set_trace()
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
