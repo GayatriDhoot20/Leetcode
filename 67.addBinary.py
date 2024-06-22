88385d00c6fff5486c7a9e55f99d4b793e8b5caf
Gayatri Dhoot <gayatri.dhoot@realpage.com>
2024-06-22 10:24:10 +0530

    Resolved comments

--- 67.addBinary.py
+++ 67.addBinary.py
@@ -5,28 +5,28 @@ class Solution(object):
         :type b: str
         :rtype: str
         """
-        if a=="0" and b=="0":
+        if a == "0" and b == "0":
             return "0"
-        bin_a = self.convert_binary(a)
-        bin_b = self.convert_binary(b)
-        add = bin_a+bin_b
+        bin_a = self.convert_decimal(a)
+        bin_b = self.convert_decimal(b)
+        add = bin_a + bin_b
 
-        sum_add = self.convert_decimal(add)
+        sum_add = self.convert_binary(add)
         return sum_add[::-1]
 
-    def convert_binary(self, var):
+    def convert_decimal(self, var):
         rev_var = str(var[::-1])
-        sum_var=0
+        sum_var = 0
         for i in range(len(rev_var)):
-            sum_var = sum_var+int(rev_var[i])*pow(2,i)
+            sum_var = sum_var + int(rev_var[i]) * pow(2, i)
         return sum_var
 
-    def convert_decimal(self, var):
-        sum_add=""
-        while (var >=1):
-            temp = var//2
-            reminder = var%2
-            sum_add = sum_add+str(reminder)
+    def convert_binary(self, var):
+        sum_add = ""
+        while var >= 1:
+            temp = var // 2
+            reminder = var % 2
+            sum_add = sum_add + str(reminder)
             var = temp
         return sum_add
 
@@ -34,4 +34,3 @@ class Solution(object):
 obj1 = Solution()
 print(obj1.addBinary("111", "11"))
 print(obj1.addBinary("0", "0"))
-
